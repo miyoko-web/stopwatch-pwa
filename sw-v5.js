dabeaba1c1e118a6188ddf7ec37df3ab040bb412
miyoko-web <oonosx4@outlook.jp>
2025-08-15 11:00:45 +0900

    Update and rename sw.js to sw-v5.js

---
+++ sw-v5.js
@@ -0,0 +1,26 @@
+// New service worker file name to bust old caches and SW registrations.
+const CACHE_NAME = 'stopwatch-pwa-v5';
+const URLS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-180.png',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(URLS)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
